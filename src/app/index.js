39fdcb34d3a2571d6c7157f3a44a4757316a4446
Ashis Jena <ashis.jena96@gmail.com>
2019-07-18 13:51:24 +0530

    Created first component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,12 @@
-alert("It works")
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+//Create a component
+var TodoComponent = React.createClass({
+    render: function(){
+        return(
+            <h1>Hellooooo</h1>
+        );
+    }
+});
+ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
